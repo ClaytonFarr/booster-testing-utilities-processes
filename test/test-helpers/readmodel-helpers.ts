725e5655c70bb15e071d1b66c7f7e9b576f208d5
Clayton Farr <ClaytonFarr@users.noreply.github.com>
2022-05-06 14:24:09 -0600

    update earlier methods to work with processes

--- test/test-helpers/readmodel-helpers.ts
+++ test/test-helpers/readmodel-helpers.ts
@@ -1,83 +1,65 @@
-import type { ApolloClient } from 'apollo-client'
+import type { UUID } from '@boostercloud/framework-types'
 import type { NormalizedCacheObject } from 'apollo-cache-inmemory'
+import type { ApolloClient } from 'apollo-client'
 import type { DocumentNode } from 'graphql'
-import { applicationUnderTest } from './infrastructure'
-import { waitForIt, looseJSONparse, isStringJSON } from './utils'
-import { faker } from '@faker-js/faker'
+import * as util from './utils'
 import gql from 'graphql-tag'
 
 // Read Model Queries
 // =====================================================================================================================
 
-export interface fieldCheck {
-  fieldName: string
-  shouldContainValue: string | number | boolean
-}
-
 export const evaluateReadModelProjection = async (
-  submitGraphQLclient: ApolloClient<NormalizedCacheObject>,
-  commandMutation: DocumentNode,
-  commandVariables: Record<string, unknown>,
-  readGraphQLclient: ApolloClient<NormalizedCacheObject>,
-  entityName: string,
-  fieldsToCheck: fieldCheck[],
+  graphQLclient: ApolloClient<NormalizedCacheObject>,
+  readModelName: string,
+  fields: Record<string, string | number | boolean | UUID>,
   sortBy?: Record<string, unknown>,
-  limitResultsTo?: number,
-  resultWaitTime = 5000
-): Promise<Record<string, unknown>> => {
-  // make test mutation for query
-  const tid = commandVariables.tid || `${entityName}-read-model-test-id-${faker.datatype.number(100)}`
-  const mutationVariables = { tid, ...commandVariables }
-  await submitGraphQLclient.mutate({ mutation: commandMutation, variables: mutationVariables })
-
-  // wait until mutation is processed
-  const primaryKey = `${entityName}-${tid}-snapshot`
-  try {
-    await waitForIt(
-      () => applicationUnderTest.query.events(primaryKey),
-      (matches) => matches?.length > 0,
-      500,
-      resultWaitTime
-    )
-  } catch (error) {
-    console.log(`💥 [Command did not do process request within ${resultWaitTime / 1000} seconds]`)
-  }
-
-  // evaluate read model projection
-  const readModelName = `${entityName}ReadModel`
-
+  limitResultsTo?: number
+): Promise<Record<string, unknown>[]> => {
   // ...grab necessary fields to check
-  const fieldNames = fieldsToCheck.map((field) => field.fieldName)
+  const readModel = util.toPascalCase(readModelName)
+  const fieldNames = []
+  const fieldItems = []
+  for (const [key, value] of Object.entries(fields)) {
+    fieldNames.push(key)
+    fieldItems.push({ fieldName: key, value })
+  }
   const fieldsToReturn = [...fieldNames].join(',')
+  const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i
 
   // ...create filter string
   let filterString = '{ '
-  fieldsToCheck.forEach((field) => {
+  fieldItems.forEach((field) => {
     // currently limited to filters from strings, numbers, and booleans
     // JSON or stringified JSON value checks will be ignored
-    if (typeof field.shouldContainValue === 'string' && !isStringJSON(field.shouldContainValue)) {
-      filterString += `${field.fieldName}: { contains: "${field.shouldContainValue}" }, `
+    if (typeof field.value !== 'string') {
+      filterString += `${field.fieldName}: { eq: ${field.value} }, `
     }
-    if (typeof field.shouldContainValue !== 'string') {
-      filterString += `${field.fieldName}: { eq: ${field.shouldContainValue} }, `
+    if (typeof field.value === 'string' && !util.valueIsTypeKeyword(field.value) && !util.isStringJSON(field.value)) {
+      filterString += `${field.fieldName}: { contains: "${field.value}" }, `
+    }
+    if (field.value.match(uuidRegex)) {
+      filterString += `${field.fieldName}: { eq: "${field.value}" }, `
+    }
+    if (util.valueIsTypeKeyword(field.value)) {
+      filterString += `${field.fieldName}: { isDefined: true }, `
     }
   })
   filterString += ' }'
 
   // ...build query
-  const filterBy = looseJSONparse(filterString)
+  const filterBy = util.looseJSONparse(filterString)
   const limitTo = limitResultsTo
   const queryVariables = createQueryVariables(filterBy, sortBy, limitTo)
-  const connectionQuery = createReadModelQuery(readModelName, fieldsToReturn)
+  const connectionQuery = createReadModelQuery(readModel, fieldsToReturn)
 
   // ...make query
-  const { data } = await readGraphQLclient.query({ query: connectionQuery, variables: queryVariables })
-  const items = data[`List${readModelName}s`].items
+  const { data } = await graphQLclient.query({ query: connectionQuery, variables: queryVariables })
+  const items = data[`List${readModel}s`].items
 
   return items
 }
 
-export const createQueryVariables = (
+const createQueryVariables = (
   filterBy?: Record<string, unknown>,
   sortBy?: Record<string, unknown>,
   limitTo?: number
@@ -89,7 +71,7 @@ export const createQueryVariables = (
   }
 }
 
-export const createReadModelQuery = (readModelName: string, fieldsToReturn?: string): DocumentNode => {
+const createReadModelQuery = (readModelName: string, fieldsToReturn?: string): DocumentNode => {
   const fields = fieldsToReturn ? fieldsToReturn : '__typename'
   return gql`
       query List${readModelName}s($filterBy: List${readModelName}Filter, $sortBy: ${readModelName}SortBy, $limitTo: Int) {
