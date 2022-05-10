2caf14940b43d36c05b6769152a491c0f93e9988
Clayton Farr <ClaytonFarr@users.noreply.github.com>
2022-05-10 07:47:58 -0600

    remove record option from values for now

--- test/test-helpers/types/process-types.ts
+++ test/test-helpers/types/process-types.ts
@@ -14,12 +14,12 @@ export interface ScheduledCommand {
 }
 export interface StateUpdate {
   entityName: string
-  values?: Record<string, string | number | boolean | UUID | Record<string, unknown>>
+  values?: Record<string, string | number | boolean | UUID>
   notValues?: Record<string, string | number | boolean | UUID> // values that should not be present in state update
 }
 export interface VisibleUpdate {
   readModelName: string
-  values?: Record<string, string | number | boolean | UUID | Record<string, unknown>>
+  values?: Record<string, string | number | boolean | UUID>
   notValues?: Record<string, string | number | boolean | UUID> // values that should not be visible
   authorized: string[] | string
 }
