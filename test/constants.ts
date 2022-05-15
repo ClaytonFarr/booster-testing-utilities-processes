a4e5ea8ba2505a41997239eb4dd124b7c1bde4b5
Clayton Farr <ClaytonFarr@users.noreply.github.com>
2022-05-15 14:37:31 -0600

    remove unused fixturesPath var

--- test/constants.ts
+++ test/constants.ts
@@ -1,4 +1,2 @@
 // `-test` suffix is appended to app name by test configuration environment
 export const testApplicationName = process.env.BOOSTER_APP_NAME ? `${process.env.BOOSTER_APP_NAME}-test` : 'myapp-test'
-
-export const fixturesPath = 'test/fixtures/'
