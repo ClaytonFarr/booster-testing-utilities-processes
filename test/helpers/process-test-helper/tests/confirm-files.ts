dc8ab344220f159ea790fd8b90c14202079c3561
Clayton Farr <ClaytonFarr@users.noreply.github.com>
2022-05-18 07:05:37 -0600

    stub out test file (temp hide filename from testing)

---
+++ test/helpers/process-test-helper/tests/confirm-files.ts
@@ -0,0 +1,32 @@
+/* eslint-disable prettier/prettier */
+import { describe, it, expect } from 'vitest'
+import * as confirm from '../confirm-files'
+
+// Test Inputs
+// ====================================================================================
+
+const authStatement01 = `@Command({
+  authorize: // Specify authorized roles here. Use 'all' to authorize anyone
+})`
+const authStatement02 = `@Command({
+  authorize: 'all',
+})`
+const authStatement03 = `@Command({
+  authorize: [User],
+})`
+const authStatement04 = `@Command({
+  authorize: [User, Admin, ThirdPartyService],
+})`
+
+
+// Tests
+// ====================================================================================
+
+describe('Process - Confirm Files', async () => {
+
+  describe('Thing', async () => {
+    //
+    it('- does', async () => expect(true).toEqual(true))
+  })
+//
+})
