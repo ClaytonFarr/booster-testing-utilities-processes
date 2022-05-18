4483940541d574b01c0195c399f15f6c1a6a1300
Clayton Farr <ClaytonFarr@users.noreply.github.com>
2022-05-18 15:20:47 -0600

    stub test file

---
+++ test/helpers/process-test-helper/tests/-todo-confirm-assertions.ts
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import { describe, it, expect } from 'vitest'
+// import * as ca from '../confirm-assertions'
+
+// Test Inputs
+// ====================================================================================
+
+// Tests
+// ====================================================================================
+
+describe('Process - Confirm Assertions', async () => {
+
+  describe('Thing', async () => {
+    //
+    it('- does', async () => expect(true).toEqual(true))
+  })
+//
+})
