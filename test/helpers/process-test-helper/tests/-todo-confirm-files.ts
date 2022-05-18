29790173252f9750ff3425c152fccf47c5ba44b1
Clayton Farr <ClaytonFarr@users.noreply.github.com>
2022-05-18 15:20:36 -0600

    rename file

--- test/helpers/process-test-helper/tests/-todo-confirm-files.ts
+++ test/helpers/process-test-helper/tests/-todo-confirm-files.ts
@@ -1,22 +1,22 @@
 /* eslint-disable prettier/prettier */
 import { describe, it, expect } from 'vitest'
-import * as confirm from '../confirm-files'
+// import * as cf from '../confirm-files'
 
 // Test Inputs
 // ====================================================================================
 
-const authStatement01 = `@Command({
-  authorize: // Specify authorized roles here. Use 'all' to authorize anyone
-})`
-const authStatement02 = `@Command({
-  authorize: 'all',
-})`
-const authStatement03 = `@Command({
-  authorize: [User],
-})`
-const authStatement04 = `@Command({
-  authorize: [User, Admin, ThirdPartyService],
-})`
+// const authStatement01 = `@Command({
+//   authorize: // Specify authorized roles here. Use 'all' to authorize anyone
+// })`
+// const authStatement02 = `@Command({
+//   authorize: 'all',
+// })`
+// const authStatement03 = `@Command({
+//   authorize: [User],
+// })`
+// const authStatement04 = `@Command({
+//   authorize: [User, Admin, ThirdPartyService],
+// })`
 
 
 // Tests
