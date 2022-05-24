b81057ff5e4dbf5d5204bcee70a0ed15d1a2bc5b
Clayton Farr <ClaytonFarr@users.noreply.github.com>
2022-05-23 19:29:53 -0600

    make inputs optional; add trigger event type; add eventValues

--- test/helpers/process-test-helper/types/process-types.ts
+++ test/helpers/process-test-helper/types/process-types.ts
@@ -10,15 +10,24 @@ export interface ActorCommand {
 export interface ScheduledCommand {
   type: 'ScheduledCommand'
   commandName: string
-  schedule: string
+  schedule: string // validation currently constrained to valid cron expressions (https://crontab.guru/); does not yet support keywords from https://docs.booster.cloud/chapters/03_booster-architecture?id=scheduling-a-command
+}
+export interface TriggerEvent {
+  // events that trigger event handlers
+  type: 'Event'
+  eventName: string
+  handlerName: string
 }
 export interface StateUpdate {
   entityName: string
+  itemId: string | number | UUID
   values?: Record<string, string | number | boolean | UUID | Record<string, unknown> | unknown[]>
   notValues?: Record<string, string | number | boolean | UUID | Record<string, unknown> | unknown[]> // values that should NOT be present in state update
 }
 export interface VisibleUpdate {
   readModelName: string
+  itemId: string | number | UUID
+  idKey?: string // field name to query for id value on readmodel; will use field 'id' if not provided
   values?: Record<string, string | number | boolean | UUID | Record<string, unknown> | unknown[]>
   notValues?: Record<string, string | number | boolean | UUID | Record<string, unknown> | unknown[]> // values that should NOT be visible
   authorized: 'all' | string[]
@@ -30,8 +39,9 @@ export interface PrecedingAction {
 }
 export interface Scenario {
   name: string
-  inputs: Record<string, string | number | boolean | UUID> // inputs are only present on Actor Commands
   precedingActions?: PrecedingAction[] // optional commands to call before scenario inputs to achieve correct state
+  inputs?: Record<string, string | number | boolean | UUID> // inputs are only present on Actor Commands
+  eventValues?: Record<string, string | number | boolean | UUID | Record<string, unknown> | unknown[]> // event values are only present on Trigger Events
   shouldBeRejected?: boolean // e.g. if scenario is invalid, confirm it should be rejected/receive error from application
   expectedStateUpdates: StateUpdate[]
   expectedVisibleUpdates?: VisibleUpdate[] // optional, a scenario may not have any visible updates
@@ -39,6 +49,6 @@ export interface Scenario {
 export interface Process {
   name: string
   confirmFiles?: boolean // if set to false, will skip steps to inspect files contents match scenario expectations
-  trigger: ActorCommand | ScheduledCommand
+  trigger: ActorCommand | ScheduledCommand | TriggerEvent
   scenarios: Scenario[]
 }
